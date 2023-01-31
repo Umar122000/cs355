89e55b7d3accca595a15fc1ca86cdcbd542f09ed
Umar122000 <uabdus@caldwell.edu>
2023-01-31 18:47:23 -0500

    week2

---
+++ script.js
@@ -0,0 +1,12 @@
+const $ = document.querySelector.bind(document);
+
+const div1= $('#div1');
+const div2= $('#div2');
+
+function btn1Clicked(){
+    div1.className = 'special'
+}
+
+function btn2Clicked(){
+    div2.className = 'special'
+}
