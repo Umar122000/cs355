89de6fd7bc14a47bee0d1ab628084495f94ad3ce
Umar122000 <uabdus@caldwell.edu>
2023-02-07 18:37:37 -0500

    todo

---
+++ todo/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const $ = document.querySelector.bind(document);
+
+const mainDiv = $('#main');
+const todoTextInput = $('#todoContent');
+const todoAddButton = $('#todoButton');
+
+function addTodo(){
+    var d = document.createElement('div');     // create div, d
+    mainDiv.appendChild( d );                  // add d to place
+    d.innerHTML = todoTextInput.value;         // add text to d
+    d.classList.add('todo');
+    d.addEventListener('click', function(){   // add onclick to d
+        d.classList.add ('completed');
+    }); 
+    todoTextInput.value ='';
+    
+}
+
+todoAddButton.addEventListener('click',addTodo);
